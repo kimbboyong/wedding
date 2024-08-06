73cec972e3d5459ab2c844b8c9d3bae0202e2321
kimbboyong <kimbboyong@gmail.com>
2024-08-06 16:16:16 +0900

    test: tmap 경로설정2

--- src/page/Location/DeepLink/components/TmapNavi/index.tsx
+++ src/page/Location/DeepLink/components/TmapNavi/index.tsx
@@ -1,69 +1,43 @@
 import { Button } from "@mui/material";
-import { useEffect, useState } from "react";
+
+declare global {
+  interface Window {
+    opera: any;
+    MSStream: any;
+  }
+}
 
 const TmapNavi = () => {
-  const [currentLocation, setCurrentLocation] = useState<{
-    latitude: number;
-    longitude: number;
-  } | null>(null);
+  const destinationX = 127.03678450961253;
+  const destinationY = 37.52158798397567;
+  // const appKey = import.meta.env.VITE_TMAP_KEY;
 
-  useEffect(() => {
-    const getCurrentLocation = () => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            setCurrentLocation({
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude,
-            });
-          },
-          (error) => {
-            console.error("위치 정보를 가져오는 데 실패했습니다:", error);
-          }
-        );
-      } else {
-        console.error("Geolocation API를 지원하지 않는 브라우저입니다.");
-      }
-    };
-    getCurrentLocation();
-  }, []);
+  const handleTmapNavi = () => {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-  const handleTmapNavi = async () => {
-    const appKey = import.meta.env.VITE_TMAP_KEY;
-    const destinationX = 127.03678450961253;
-    const destinationY = 37.52158798397567;
+    const isAndroid = /android/i.test(userAgent);
+    const isIOS =
+      /iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream;
 
-    const url = "https://apis.openapi.sk.com/tmap/routes?version=1";
+    const tmapUrl = `tmap://route?ep=${destinationX},${destinationY}`;
 
-    const data = {
-      startX: currentLocation ? currentLocation.longitude : 127.10323758,
-      startY: currentLocation ? currentLocation.latitude : 37.36520202,
-      endX: destinationX,
-      endY: destinationY,
-    };
+    if (isAndroid) {
+      window.location.href = tmapUrl;
 
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          appKey: appKey,
-        },
-        body: JSON.stringify(data),
-      });
+      setTimeout(() => {
+        window.location.href =
+          "https://play.google.com/store/apps/details?id=com.skt.tmap.ku";
+      }, 2000);
+    } else if (isIOS) {
+      window.location.href = tmapUrl;
 
-      if (!response.ok) {
-        throw new Error("네트워크 응답에 문제가 있습니다.");
-      }
-
-      const result = await response.json();
-      console.log("result?", result);
-    } catch (error) {
-      console.error("에러 발생:", error);
+      setTimeout(() => {
+        window.location.href = "https://apps.apple.com/kr/app/tmap/id494202123";
+      }, 2000);
+    } else {
+      alert("지원하지 않는 플랫폼입니다.");
     }
   };
-  console.log(currentLocation);
 
   return (
     <Button
@@ -76,7 +50,6 @@ const TmapNavi = () => {
         border: "1px solid #ccc",
       }}
       onClick={handleTmapNavi}
-      disabled={!currentLocation}
     >
       <figure>
         <img src="/images/ico-tmap.png" alt="" />
