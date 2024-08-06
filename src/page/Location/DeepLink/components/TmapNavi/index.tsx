c81e7556e49299ee059ffdf00500b4bea0bc848f
kimbboyong <kimbboyong@gmail.com>
2024-08-06 16:38:47 +0900

    test: tmap test3

--- src/page/Location/DeepLink/components/TmapNavi/index.tsx
+++ src/page/Location/DeepLink/components/TmapNavi/index.tsx
@@ -1,41 +1,17 @@
 import { Button } from "@mui/material";
 
-declare global {
-  interface Window {
-    opera: any;
-    MSStream: any;
-  }
-}
-
 const TmapNavi = () => {
-  const destinationX = 127.03678450961253;
-  const destinationY = 37.52158798397567;
-  const appKey = import.meta.env.VITE_TMAP_KEY;
+  const destination = {
+    name: "마리아쥬 스퀘어",
+    x: 127.03678450961253,
+    y: 37.52158798397567,
+  };
   const handleTmapNavi = () => {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    const tmapUrl = `tmap://route?ep=${destination.x},${
+      destination.y
+    }&name=${encodeURIComponent(destination.name)}`;
 
-    const isAndroid = /android/i.test(userAgent);
-    const isIOS =
-      /iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream;
-
-    const tmapUrl = `tmap://route?ep=${destinationX},${destinationY}`;
-    appKey;
-    if (isAndroid) {
-      window.location.href = tmapUrl;
-
-      setTimeout(() => {
-        window.location.href =
-          "https://play.google.com/store/apps/details?id=com.skt.tmap.ku";
-      }, 2000);
-    } else if (isIOS) {
-      window.location.href = tmapUrl;
-
-      setTimeout(() => {
-        window.location.href = "https://apps.apple.com/kr/app/tmap/id494202123";
-      }, 2000);
-    } else {
-      alert("지원하지 않는 플랫폼입니다.");
-    }
+    window.location.href = tmapUrl;
   };
 
   return (
