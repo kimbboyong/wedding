a16719d971ac42108ecd84615b52192f4b94876f
kimbboyong <kimbboyong@gmail.com>
2024-08-08 11:26:10 +0900

    update: evn update/modal css정리

--- src/components/CommonModal/index.tsx
+++ src/components/CommonModal/index.tsx
@@ -31,15 +31,11 @@ const CommonModal = ({ isOpen, closeModal, children }: Props) => {
 export default CommonModal;
 
 const style = {
-  // display: "flex",
-  // justifyContent: "center",
-  // alignItems: "center",
   position: "absolute" as "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: "90%",
-  // height: "500px",
   bgcolor: "background.paper",
   boxShadow: 24,
   p: 2,
